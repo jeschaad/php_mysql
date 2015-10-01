d2b2a09921dc062593d326f5005774d175afac15
Jim Schaad <jeschaad@gmail.com>
2015-09-30 23:41:28 -0400

    Updated welcome.php for exercise 5

--- welcome.php
+++ welcome.php
@@ -8,8 +8,10 @@
 	</head>
 	<body>
 		<?php
-			$firstname = $_GET['firstname'];
-			$lastname = $_GET['lastname'];
+#			$firstname = $_GET['firstname'];
+#			$lastname = $_GET['lastname'];
+			$firstname = $_POST['firstname'];
+			$lastname = $_POST['lastname'];
 			echo 'Welcome to our website, ' . 
 			htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' . 
 			htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
