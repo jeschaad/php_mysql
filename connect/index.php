bb4595b7eb5c87eed372604bcea2acea9bfad2de
jeschaad <jeschaad@users.noreply.github.com>
2015-10-25 23:28:16 -0400

    Create index.php

---
+++ connect/index.php
@@ -0,0 +1,23 @@
+<?php
+$link = mysqli_connect('localhost', 'root', '');
+if (!$link) {
+  $output = 'Unable to connect to the database server.';
+  include 'output.html.php';
+  exit();
+}
+
+if (!mysqli_set_charset($link, 'utf8')) {
+  $output = 'Unable to set database connection encoding.';
+  include 'output.html.php';
+  exit();
+}
+
+if (!mysqli_select_db($link, 'librecoin')) {
+  $output = 'Unable to locate the librecoin database.';
+  include 'output.html.php';
+  exit();
+}
+
+$output = 'Database connection established.';
+include 'output.html.php';
+?>
