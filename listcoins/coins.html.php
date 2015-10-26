b28fb6bf74eb215e4faeb71f5bdd04addcea6b1a
jeschaad <jeschaad@users.noreply.github.com>
2015-10-26 15:37:45 -0400

    Create coins.html.php

---
+++ listcoins/coins.html.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<title>List of Coins</title>
+		<meta http-equiv="content-type"
+			content="text/html; charset=utf-8"/>
+	</head>
+	<body>
+		<p>Here are all the coins in the database:</p>
+			<?php foreach ($coins as $coin): ?>
+			  <blockquote><p>
+			  <?php echo htmlspecialchars($joke, ENT_QUOTES, 'UTF-8'); ?>
+		  </p></blockquote>
+		<?php endforeach; ?>
+	</body>
+</html>
