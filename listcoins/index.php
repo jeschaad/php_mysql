ce270c7549dee7ec8a9920588eecf13fff3c488f
jeschaad <jeschaad@users.noreply.github.com>
2015-10-26 22:15:42 -0400

    All $output now $error and corrected query names

--- listcoins/index.php
+++ listcoins/index.php
@@ -1,30 +1,30 @@
 <?php
 $link = mysqli_connect('localhost', 'root', 'password');
 if (!$link) {
-  $output = 'Unable to connect to the database server.';
+  $error = 'Unable to connect to the database server.';
   include 'error.html.php';
   exit();
 }
 if (!mysqli_set_charset($link, 'utf8')) {
-  $output = 'Unable to set database connection encoding.';
-  include 'output.html.php';
+  $error = 'Unable to set database connection encoding.';
+  include 'error.html.php';
   exit();
 }
 if (!mysqli_select_db($link, 'librecoin')) {
-  $output = 'Unable to locate the librecoin database.';
+  $error = 'Unable to locate the librecoin database.';
   include 'error.html.php';
   exit();
 }
 
-$result = mysqli_query($link, 'SELECT type FROM coins');
+$result = mysqli_query($link, 'SELECT item FROM librecoin');
 if (!$result) {
-  $error = 'Error fetching coins: ' . mysqli_error($link);
+  $error = 'Error fetching items: ' . mysqli_error($link);
   include 'error.html.php';
   exit();
 }
 
 while ($row = mysqli_fetch_array($result)) {
-  $coins[] = $row['type'];
+  $coins[] = $row['item'];
 }
 include 'coins.html.php';
 ?>
